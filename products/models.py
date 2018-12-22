920b6219dad3efea98863dc17d4ec171a3ad84bd
Chip <chip.myersjr@gmail.com>
2018-12-21 20:20:19 -0800

    add model

---
+++ products/models.py
@@ -0,0 +1,11 @@
+from application import db
+from datetime import  datetime
+
+
+class Product(db.Document):
+    id = db.StringField(db_field="id")
+    title = db.StringField(db_field="title")
+    product_type = db.StringField(db_field="product_type")
+    vendor = db.StringField(db_field="vendor")
+    created_at = db.DateTimeField(default=datetime.utcnow)
+    updated_at = db.DateTimeField(default=datetime.utcnow)
