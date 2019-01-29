83a5fed0b2ec5e988e3b35c0ef9dbb759d33eee1
Chip <chip.myersjr@gmail.com>
2019-01-28 20:40:03 -0800

    add giftcard amount to invoice model

--- invoice/models.py
+++ invoice/models.py
@@ -9,9 +9,10 @@ from product.models import Product
 
 class Invoice(db.Document):
     invoice_id = db.StringField(db_field="invoice_id", primary_key=True)
-    customer_id = db.ReferenceField(Customer, db_field="customer_id")
-    cart_id = db.ReferenceField(Cart, db_field="cart_id")
+    customer = db.ReferenceField(Customer, db_field="customer_id")
+    cart = db.ReferenceField(Cart, db_field="cart_id")
     state = db.StringField(default="open")
+    gift_card_used_amount = db.DecimalField(default=0)
     created_at = db.DateTimeField(default=datetime.now())
     closed_at = db.DateTimeField()
 
@@ -22,9 +23,9 @@ class Invoice(db.Document):
 
 class InvoiceLineItem(db.Document):
     invoice_line_item_id = db.StringField(db_field="invoice_line_item_id", primary_key=True)
-    invoice_id = db.ReferenceField(Invoice, db_field="invoice_id")
-    cart_item_id = db.ReferenceField(CartItem, db_field="cart_item_id")
-    product_id = db.ReferenceField(Product, db_field="product_id")
+    invoice = db.ReferenceField(Invoice, db_field="invoice_id")
+    cart_item = db.ReferenceField(CartItem, db_field="cart_item_id")
+    product = db.ReferenceField(Product, db_field="product_id")
     quantity = db.IntField()
     unit_amount = db.DecimalField()
     total_amount = db.DecimalField()
